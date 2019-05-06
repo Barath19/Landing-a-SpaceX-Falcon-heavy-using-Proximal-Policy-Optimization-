97840412a27818976afef92ce1218306f5255f57
Bharath Kumar <30743936+Barath19@users.noreply.github.com>
2019-05-06 22:03:28 +0530

    Update ppo_RL_pretrained.py

--- ppo_RL_pretrained.py
+++ ppo_RL_pretrained.py
@@ -1,11 +1,13 @@
-import gym
+#based on stable baselines implementation https://stable-baselines.readthedocs.io/en/master/modules/ppo2.html
 
+import gym
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines import PPO2
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
-
 from gym.envs.registration import registry, register, make, spec
+
+#registering custom environment
 register(
     id='RocketLander-v0',
     entry_point='rocketlander.rocket_lander:RocketLander',#gym.envs.box2d:
@@ -21,9 +23,11 @@ env = DummyVecEnv([lambda: env])
 
 
 # Load the trained agent
+#ppo2_RocketLander-v0_20000000_2019-05-05 03/26/38.pkl
 model = PPO2.load("./model/ppo2_RocketLander_15000000.pkl")
 
-# Enjoy trained agent
+
+#Trained agent in action
 obs = env.reset()
 while True:
     action, _states = model.predict(obs)
